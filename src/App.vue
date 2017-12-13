9bd00296f64225e557e64a89373eec8e3f0bbfb3
brapastor <brapastor@gmail.com>
2017-12-13 00:39:26 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  #app
+    h1 {{msg}}
+    p {{1+1}}
+    p {{'hola' + ' Mundillo'}}
+    p {{person.name}}
+    p {{person.name.toUpperCase()}}
+    p {{JSON.stringify(person)}}
+
+</template>
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'Hola vue!',
+        person: {
+          name: 'brayan'
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "./scss/main";
+</style>
