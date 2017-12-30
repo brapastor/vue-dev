4a940748d7da60533687d26873fad34ae9c51858
brapastor <brapastor@gmail.com>
2017-12-29 19:48:18 -0500

    agregado de components

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,48 @@
 <template lang="pug">
   #app
-    h1 {{msg}}
-    p {{1+1}}
-    p {{'hola' + ' Mundillo'}}
-    p {{person.name}}
-    p {{person.name.toUpperCase()}}
-    p {{JSON.stringify(person)}}
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+                  type="text",
+                  placeholder="Buscar Canciones",
+                  v-model='searchQuery'
+          )
+
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+      .container
+        p
+          small {{searchMessage}}
+      .container.results
+        .column(v-for="t in tracks")
+          | {{t.name}}-{{t.artists[0].name}}
+
 
 </template>
 <script>
+  import trackService from './services/track'
+
   export default {
     name: 'app',
     data () {
       return {
-        msg: 'Hola vue!',
-        person: {
-          name: 'brayan'
-        }
+        searchQuery: '',
+        tracks: []
+      }
+    },
+    computed: {
+      searchMessage () {
+        return `Encontrados ${this.tracks.length}`
+      }
+    },
+    methods: {
+      search () {
+        if (!this.searchQuery) { return }
+        trackService.search(this.searchQuery)
+          .then(res => {
+            this.tracks = res.tracks.items
+          })
       }
     }
   }
@@ -24,4 +50,7 @@
 
 <style lang="scss">
   @import "./scss/main";
+  .results{
+    margin-top: 50px;
+  }
 </style>
