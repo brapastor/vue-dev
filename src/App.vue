6a1c3a6ee82c0ee2d09c0a348bd6087ded73b495
brapastor <brapastor@gmail.com>
2018-01-02 19:52:52 -0500

    agregado de componentes

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,86 @@
 <template lang="pug">
   #app
-    section.section
-      nav.nav.has-shadow
+    pm-header
+
+    pm-notification(v-show="showNotification")
+      p(slot="body") No se encontraron resultados
+
+    pm-loader(v-show="isLoading")
+    section.section(v-show="!isLoading")
+      nav.nav
         .container
           input.input.is-large(
                   type="text",
                   placeholder="Buscar Canciones",
                   v-model='searchQuery'
           )
-
           a.button.is-info.is-large(@click="search") Buscar
           a.button.is-danger.is-large &times;
       .container
         p
           small {{searchMessage}}
       .container.results
-        .column(v-for="t in tracks")
-          | {{t.name}}-{{t.artists[0].name}}
+        .columns.is-multiline
+          .column.is-one-quarter(v-for="t in tracks")
+            pm-track(
+            :class="{'is-active': t.id == selectedTrack}",
+            :track="t",
+            @select="setSelectedTrack")
+
+    pm-foooter
 
 
 </template>
 <script>
-  import trackService from './services/track'
+  import trackService from '@/services/track'
+  import PmFoooter from '@/components/layout/Footer.vue'
+  import PmHeader from '@/components/layout/Header.vue'
+  import PmTrack from '@/components/Track.vue'
+  import PmLoader from '@/components/shared/Loader.vue'
+  import PmNotification from '@/components/shared/Notification.vue'
 
   export default {
     name: 'app',
     data () {
       return {
         searchQuery: '',
-        tracks: []
+        tracks: [],
+        isLoading: false,
+        selectedTrack: '',
+        showNotification: false
       }
     },
+
+    components: { PmFoooter, PmHeader, PmTrack, PmLoader, PmNotification },
     computed: {
       searchMessage () {
         return `Encontrados ${this.tracks.length}`
       }
     },
+    watch: {
+      showNotification () {
+        if (this.showNotification) {
+          setTimeout(() => {
+            this.showNotification = false
+          }, 3000)
+        }
+      }
+    },
     methods: {
       search () {
         if (!this.searchQuery) { return }
+        this.isLoading = true
         trackService.search(this.searchQuery)
           .then(res => {
+            this.showNotification = res.tracks.total === 0
             this.tracks = res.tracks.items
+            this.isLoading = false
           })
+      },
+      setSelectedTrack (id) {
+        this.selectedTrack = id
       }
+
     }
   }
 </script>
@@ -52,5 +89,8 @@
   @import "./scss/main";
   .results{
     margin-top: 50px;
+  }
+  .is-active{
+    border: 3px solid #23d160;
   }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  section.hero.is-success
+    .hero-head
+      header.nav
+        .container
+          .nav-left
+            .nav-item
+              strong 📻 platzi music
+          .nav-right.nav-menu
+
+    .hero-body
+      .container.has-text-centered
+        h1.title platzi music
+        h2.subtitle canciones que estan buenas
+
+</template>
